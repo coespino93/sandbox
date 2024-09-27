8d665f3df4431ac6d3906a7030f79b539a139297
coespino93 <coespino@syr.edu>
2024-09-26 20:09:35 -0400

    Site initial structure

---
+++ script.js
@@ -0,0 +1,5 @@
+// Placeholder JavaScript file.
+// You can add interactivity to buttons or navigation in this file.
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Carlos's Sandbox site loaded successfully!");
+  });
